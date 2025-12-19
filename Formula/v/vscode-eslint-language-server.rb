f73664a48045aec1388efe7d8e77ff7f452c8ab0
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-12-20 01:32:26 +0900

    vscode-eslint-language-server: add formula for VSCode ESLint Language Server
    Add a new formula to provide the Language Server Protocol implementation
    for ESLint from the official vscode-eslint repository. This allows
    editor integration with ESLint through LSP-compatible editors.

---
+++ Formula/v/vscode-eslint-language-server.rb
@@ -0,0 +1,53 @@
+require "language/node"
+
+class VscodeEslintLanguageServer < Formula
+  desc "Language server for ESLint"
+  homepage "https://github.com/microsoft/vscode-eslint"
+  url "https://github.com/microsoft/vscode-eslint/archive/refs/tags/release/3.0.20.tar.gz"
+  sha256 "f002c40e8fbf3186abe65158a206e18970c572e38e1872354ba074e0e2d76ad6"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{release/v?(\d+(?:\.\d+)*)}i)
+  end
+
+  depends_on "node"
+
+  def install
+    system "npm", "ci"
+    system "npm", "run", "compile:server"
+
+    libexec.install Dir["*"]
+
+    # Create a wrapper script that explicitly invokes node with the server script
+    (bin/"vscode-eslint-language-server").write <<~EOS
+      #!/bin/bash
+      export PATH="#{Formula["node"].opt_bin}:$PATH"
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/server/out/eslintServer.js" "$@"
+    EOS
+  end
+
+  test do
+    require "open3"
+
+    json = <<~JSON
+      {
+        "jsonrpc": "2.0",
+        "id": 1,
+        "method": "initialize",
+        "params": {
+          "rootUri": null,
+          "capabilities": {}
+        }
+      }
+    JSON
+
+    Open3.popen3("#{bin}/vscode-eslint-language-server", "--stdio") do |stdin, stdout|
+      stdin.write "Content-Length: #{json.size}\r\n\r\n#{json}"
+      sleep 2
+      assert_match(/^Content-Length: \d+/i, stdout.readline)
+    end
+  end
+end
